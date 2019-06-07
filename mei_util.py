be7c0092c0cbf1e10cd4eb92512ebce13de17f75
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-06-07 13:42:02 -0400

    Put custos inside sb when possible

--- mei_util.py
+++ mei_util.py
@@ -19,6 +19,13 @@ def staff_based_to_sb(original_file, modified_file):
                 sb = ElementTree.SubElement(container, '{http://www.music-encoding.org/ns/mei}sb')
                 sb.set('{http://www.w3.org/XML/1998/namespace}id', staff.get('{http://www.w3.org/XML/1998/namespace}id'))
                 sb.set('facs', staff.get('facs'))
+                # Check for custos
+                if len(container) > 2:
+                    if container[-2].tag == '{http://www.music-encoding.org/ns/mei}custos':
+                        sb.append(container[-2])
+                        container.remove(container[-2])
+                    else:
+                        print(container[-2].tag)
 
                 container.extend(list(layer))
 
@@ -26,6 +33,20 @@ def staff_based_to_sb(original_file, modified_file):
         section.clear()
         section.set('{http://www.w3.org/XML/1998/namespace}id', section_id)
         section.append(new_staff)
+
+        # Handle split
+        '''
+        for second_half in section.findall('.//*[@follows]'):
+            second_half_id = second_half.get('{http://www.w3.org/XML/1998/namespace}id')
+            first_half_id = second_half.get('follows')
+            first_half = section.find('.//*[@{http://www.w3.org/XML/1998/namespace}id=\'' + first_half_id + '\']')
+            if first_half is None:
+                print("Could not find syllable with xml:id " + first_half_id)
+                continue
+            first_half.extend(list(second_half))
+            section.find('.//*[@{http://www.w3.org/XML/1998/namespace}id=\'' + second_half_id + '\']/..').remove(second_half)
+            #second_half.find('./..').remove(second_half)
+        '''
 
     mei_tree.write(modified_file, encoding='utf-8', xml_declaration=True)
 
@@ -47,6 +68,9 @@ def sb_based_to_staff(original_file, modified_file):
                     if elem.tag == '{http://www.music-encoding.org/ns/mei}sb':
                         if container is not None:
                             if len(list(container)) > 0:
+                                if len(list(elem)) > 0:
+                                    # Move custos
+                                    container.extend(list(elem))
                                 # Add our staff to the list
                                 staff_store.append(new_staff)
                                 new_staff = ElementTree.Element('{http://www.music-encoding.org/ns/mei}staff', {'n': str(count)})
